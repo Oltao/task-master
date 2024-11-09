eae6caaafc83ff9168037a9699134ce0dcf499c3
Olajire Taofeek <taofeeqolajire@gmail.com>
2024-11-09 18:35:57 +0100

    connect db

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 import express from "express";
+import userRoute from "./routes/user.route.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use("/auth", userRoute);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  await connectDB();
   console.log(`Server started on http://localhost:${port}`);
 });
